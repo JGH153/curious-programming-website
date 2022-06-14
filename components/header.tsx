75474a38947a414a866cafe50c887de726f99e80
JGH153 <jan.greger@gmail.com>
2022-06-14 10:00:30 +0200

    improved mobile

--- components/header.tsx
+++ components/header.tsx
@@ -28,16 +28,11 @@ export function Header() {
       <div className="p-5 lg:p-10">
         <nav className="text-primary mx-auto flex max-w-8xl items-center justify-between">
           <div>
-            {/* <a
-          className="text-primary underlined focus:outline-none block whitespace-nowrap text-2xl font-medium transition"
-          href="/"
-        > */}
             <Link href={"/"}>
               <a className="text-primary underlined focus:outline-none block whitespace-nowrap text-3xl md:text-4xl lg:text-5xl font-medium transition">
                 Curious Programming 🤔
               </a>
             </Link>
-            {/* </a> */}
           </div>
           <ul className="hidden md:flex">
             {pages.map((page, i) => (
@@ -50,17 +45,6 @@ export function Header() {
                 </Link>
               </li>
             ))}
-            {/* <li className="px-5 py-2">
-          <a
-            className="underlined focus:outline-none block whitespace-nowrap text-lg font-medium hover:text-team-current focus:text-team-current active text-team-current"
-            href="/blog"
-          >
-            Blog
-          </a>
-          <span className="text-lg font-medium">Blog</span>
-        </li>
-        <li className="px-5 py-2">Videos</li>
-        <li className="px-5 py-2">About</li> */}
           </ul>
           <div className="pl-5">
             <HamburgerButton
@@ -72,14 +56,22 @@ export function Header() {
       </div>
 
       {open && (
-        <ul className="absolute flex flex-col items-center justify-center py-8 mt-2 space-y-6 drop-shadow-lg left-6 right-6 font-bold bg-background-dark">
-          {/* <ul className="absolute flex-col items-center self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md"> */}
+        <ul className="absolute flex flex-col items-center justify-center py-8 mt-2 space-y-6 drop-shadow-lg left-6 right-6 font-bold z-10 bg-background-dark md:hidden">
           {pages.map((page, i) => (
             <li
               className="px-5 py-2"
               key={page.label}
             >
-              <span className="text-lg font-medium underlined">{page.label}</span>
+              <span className="text-lg font-medium underlined">
+                <Link href={page.href}>
+                  <a
+                    className="text-xl font-medium underlined"
+                    onClick={() => setOpen(false)}
+                  >
+                    {page.label}
+                  </a>
+                </Link>
+              </span>
             </li>
           ))}
         </ul>
--- components/layout.tsx
+++ components/layout.tsx
@@ -5,12 +5,11 @@ import { Header } from "./header";
 export function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex justify-center align-top">
-      <div className="max-w-4xl">
+      <div className="max-w-4xl p-3 lg:p-0">
         <Header />
         <main>{children}</main>
         <Footer />
       </div>
-      {/* <Footer /> */}
     </div>
   );
 }
