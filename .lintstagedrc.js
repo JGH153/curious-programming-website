807fb412e597bd2ee5e4b61517ccbc5be7f9afa6
JGH153 <jan.greger@gmail.com>
2022-07-05 13:45:48 +0200

    added lint and prettier pre-commit rule

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,8 +3,8 @@
 const path = require("path");
 
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(" --file ")}`;
+  `npm run lint-strict -- --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(" --file ")}`;
 
 module.exports = {
-  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  "*.{js,jsx,ts,tsx}": ["npm run prettier-fix", buildEslintCommand],
 };
